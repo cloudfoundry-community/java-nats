dfc587dd0b1547b5a083ade287614df9186911e1
Mike Heath <elcapo@gmail.com>
2012-04-27 10:28:13 -0600

    Updated pom to version 0.2.

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,4 +1,5 @@
 import nats.CompletionHandler;
+import nats.NatsLogger;
 import nats.client.Message;
 import nats.client.MessageHandler;
 import nats.client.Nats;
@@ -10,22 +11,26 @@ import nats.NatsFuture;
 public class Test {
 	public static void main(String[] args) throws Exception {
 		final Nats nats = new Nats.Builder()
-//				.addHost(new URI("nats://nats:nats@api.vcap.ldschurch.org"))
+//				.addHost("nats://nats:nats@api.vcap.ldschurch.org")
 				.addHost("nats://nats:nats@localhost")
 //				.addHost("nats://nats:efcc5ac8e44ee5b4@api.mikeheath.cloudfoundry.me")
+//				.logger(NatsLogger.DEBUG_LOGGER)
 				.connect();
-		nats.subscribe(">").addMessageHandler(new MessageHandler() {
-			@Override
-			public void onMessage(Message message) {
-				System.out.println("** onMessage ** " + message);
-			}
-		});
-		nats.publish("foo", "message").addCompletionHandler(new CompletionHandler() {
-			@Override
-			public void onComplete(NatsFuture future) {
-				System.out.println("Published message! " + future.isSuccess());
-			}
-		});
+//		nats.subscribe(">").addMessageHandler(new MessageHandler() {
+//			@Override
+//			public void onMessage(Message message) {
+//				System.out.println(message);
+//			}
+//		});
+		for(Message message : nats.subscribe(">")) {
+			System.out.println(message);
+		}
+//		nats.publish("foo", "message").addCompletionHandler(new CompletionHandler() {
+//			@Override
+//			public void onComplete(NatsFuture future) {
+//				System.out.println("Published message! " + future.isSuccess());
+//			}
+//		});
 		//nats.close();
 	}
 }
