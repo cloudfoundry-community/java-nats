9b4ba5d06eeb7a0348571d552d15930db5dabf77
Mike Heath <elcapo@gmail.com>
2012-09-24 12:49:15 -0600

    Moved vcap integration to different project.

---
+++ client-spring/src/test/java/Test.java
@@ -0,0 +1,20 @@
+import org.springframework.util.MethodInvoker;
+
+/**
+ * @author Mike Heath <heathma@ldschurch.org>
+ */
+public class Test {
+
+	public void test(String s, Integer i) {
+		System.out.println("s = [" + s + "], i = [" + i + "]");
+	}
+
+	public static void main(String[] args) throws Exception {
+		MethodInvoker invoker = new MethodInvoker();
+		invoker.setTargetObject(new Test());
+		invoker.setTargetMethod("test");
+		invoker.setArguments(new Object[] {Integer.valueOf(1), "Hi"});
+		invoker.prepare();
+		invoker.invoke();
+	}
+}
